0b28db5cf0c229b978462b8c7e7d65c3e0050102
aakash-sharma25 <as024harma@gmail.com>
2025-02-06 18:26:11 +0530

    fixed allignment

--- components/HomePageComponent/WhyJoin.js
+++ components/HomePageComponent/WhyJoin.js
@@ -13,28 +13,46 @@ export default function WhyJoin() {
           </span>
         </div>
 
-        <div className=" flex flex-wrap items-center justify-center gap-2 p-1 font-medium text-black my-10">
-          <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg">
-            {" "}
-            Showcase Talent
+        <div className=" flex flex-col items-center justify-center gap-y-2 font-medium text-black my-10">
+
+          <div className=" text-center flex items-center justify-center  w-[95vw]  gap-2">
+            <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg flex-auto ">
+              Showcase Talent
+            </div>
+            <div className=" bg-[#D73970] px-2 py-1 rounded-lg flex-auto">
+              Get Feedback
+            </div>
+            <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg flex-auto">
+              {" "}
+              Learn
+            </div>
           </div>
-          <div className=" bg-[#D73970] px-2 py-1 rounded-lg">
-            {" "}
-            Get Feedback
+
+          <div className="text-center flex items-center justify-center w-[95vw]  gap-2 ">
+            <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg flex-auto">
+              Build Confidence
+            </div>
+            <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg flex-auto">
+              {" "}
+              Reward
+            </div>
+            <div className=" bg-[#D73970] px-2 py-1 rounded-lg flex-auto">
+              Challenges
+            </div>
           </div>
-          <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg"> Learn</div>
-          <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg">
-            {" "}
-            Build Confidence
+
+          <div className=" text-center flex items-center justify-center w-[95vw]  gap-2">
+            <div className=" bg-[#D73970] px-2 py-1 rounded-lg flex-auto ">
+              {" "}
+              Grow
+            </div>
+            <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg flex-auto ">
+              Gain Regognition
+            </div>
+            <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg flex-auto ">
+              New Skills
+            </div>
           </div>
-          <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg"> Reward</div>
-          <div className=" bg-[#D73970] px-2 py-1 rounded-lg"> Challenges</div>
-          <div className=" bg-[#D73970] px-2 py-1 rounded-lg"> Grow</div>
-          <div className=" bg-[#FFC56F] px-2 py-1 rounded-lg">
-            {" "}
-            Gain Regognition
-          </div>
-          <div className=" bg-[#00AFAA] px-2 py-1 rounded-lg"> New Skills</div>
         </div>
       </div>
     </>
