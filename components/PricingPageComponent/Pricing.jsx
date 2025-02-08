56e815a50c78f97d2b62ef706773ceff5d768593
aakash-sharma25 <as024harma@gmail.com>
2025-02-08 20:57:47 +0530

    adding pricing

---
+++ components/PricingPageComponent/Pricing.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PricingCards from "./PricingCards";
+
+export default function Pricing() {
+  return (
+    <div >
+      <div className=" text-center px-3 mt-5 pb-10">
+        <p className="  text-white text-3xl font-semibold">
+          Flexible Choices,
+          <span className=" text-[#D73970] "> Unlimited Opportunities!</span>
+        </p>
+        <p className=" text-sm  text-white mt-2 sm:text-base" >
+          Pick the best way to shine! Choose one or dual competition for
+          recognition, rewards, and growth.
+        </p>
+        <PricingCards/>
+      </div>
+    </div>
+  );
+}
