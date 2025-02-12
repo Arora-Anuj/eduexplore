3039e88f622ab361dfeaab1212d8e09ebd5fcb86
aakash-sharma25 <as024harma@gmail.com>
2025-02-12 20:22:45 +0530

    added challenges component

--- components/HomePageComponent/Challenges.js
+++ components/HomePageComponent/Challenges.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import talent1 from "@/public/asset/talent1.png";
+import chal from "@/public/asset/chal.png";
 import winner from "@/public/asset/winner.png";
 import trophy from "@/public/asset/trophy.png";
 import Link from "next/link";
@@ -11,56 +11,25 @@ const publicaSans = localFont({
   src: "../../public/fonts/PublicaSans.otf",
 });
 
-const challenges = [
-  {
-    title: "Color Carnival",
-    description:
-      "Celebrating creativity and imagination through colorful masterpieces.",
-    category: "Drawing",
-    winner: "Riya",
-    // image: "/color-carnival.jpg",
-    image: talent1,
-    bgColor: "bg-yellow-400",
-  },
-  {
-    title: "Dancing Star",
-    description:
-      "Theme Options:\n1. Bollywood Beats\n2. Retro Vibes\n3. Seasons in Dance",
-    category: "Dancing",
-    winner: "Riya",
-    // image: "/dancing-star.jpg",
-    image: talent1,
-    bgColor: "bg-green-400",
-  },
-  {
-    title: "Singing Star",
-    description: "A singing competition where young voices shine!",
-    category: "Drawing",
-    winner: "Charvi",
-    // image: "/singing-star.jpg",
-    image: talent1,
-    bgColor: "bg-pink-400",
-  },
-];
-
 export default function Challenges() {
   return (
     <>
-      <div className=" pt-[21px] bg-[#EEEFFC] w-full flex flex-col items-center justify-center md:hidden">
-        <div className="  text-center w-11/12">
-          <span className={`text-[#1b1d30] text-[26px] font-medium leading-[25px] ${publicaSans.className}`}>
+      <div className=" pt-[21px] bg-[#EEEFFC] w-full flex flex-col items-center justify-center md:bg-[#1B1D30]">
+        <div className="  text-center w-11/12 md:w-7/12 lg:w-7/12 md:my-[50px] my-[20px]">
+          <span className={`text-[#1b1d30] text-[26px] md:text-[32px] lg:text-[52px] lg:leading-[50px] md:leading-[30px] font-medium leading-[25px] ${publicaSans.className}  md:text-[#eeeffc]`}>
             Take a look at the
           </span>
-          <span className={`text-[#d63970] text-[26px] font-medium leading-[25px] ${publicaSans.className}`}>
+          <span className={`text-[#d63970] text-[26px] md:text-[32px] lg:text-[52px] lg:leading-[50px] md:leading-[30px] font-medium leading-[25px] ${publicaSans.className}`}>
             {" "}
             Most Exciting Challenges
           </span>
-          <span className={`text-[#1b1d30] text-[26px] font-medium leading-[25px] ${publicaSans.className}`}>
+          <span className={`text-[#1b1d30] md:text-[#eeeffc] text-[26px] md:text-[32px] lg:text-[52px] lg:leading-[50px] md:leading-[30px] font-medium leading-[25px] ${publicaSans.className}`}>
             {" "}
             and Previous Competition Winners
           </span>
         </div>
-        <Image src={winner} alt="winner" className=" w-auto h-[450px] mt-[20px]" />
+        <Image src={winner} alt="winner" className=" w-auto h-[450px] mt-[20px] md:hidden" />
+        <Image src={chal} alt="winner" className=" w-[90%] h-[390px] mt-[20px] hidden md:block" />
         <Link
           href={"/pricing"}
           className={` my-[30px] bg-[#5259AA] font-bold text-white py-4 px-8 rounded-lg text-lg`}
